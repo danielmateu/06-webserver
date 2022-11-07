ebc64842b88c3d257d35c510a8a179c0e7f86e85
Daniel Mateu <danielmateu86@gmail.com>
2022-11-07 16:45:37 +0100

    Primeros contactos con express

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 
 
 const express = require('express')
-const app = express()
+const app = express();
+const port = 8080;
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+app.get('/', (req, res) => {
+    res.send('Home')
 })
 
-app.listen(8080)
+app.get('/hola-mundo', (req, res) => {
+    res.send('Hello World en su respectiva ruta')
+});
+
+app.get('*', (req, res) => {
+    res.send('404 | Page not found')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
