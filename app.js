c253397955416da073bec9ad9029322b098abae6
Daniel Mateu <danielmateu86@gmail.com>
2022-11-07 16:55:59 +0100

    Servir contenido estático

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ const express = require('express')
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-    res.send('Home')
-})
+// Servir contenido estático
+app.use(express.static('public'));
 
 app.get('/hola-mundo', (req, res) => {
     res.send('Hello World en su respectiva ruta')
 });
 
 app.get('*', (req, res) => {
-    res.send('404 | Page not found')
+    res.sendFile(__dirname + '/public/404.html')
 })
 
 app.listen(port, () => {
