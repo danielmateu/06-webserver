0511d3a9c37d9d0c33691d52d5bd081a8ffa1ceb
Daniel Mateu <danielmateu86@gmail.com>
2022-11-08 12:32:01 +0100

    instalacion dotenv y creando las variables de entorno

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 
 const hbs = require('hbs');
-const express = require('express')
+const express = require('express');
+require('dotenv').config()
+
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 
 
 //Handlebars
